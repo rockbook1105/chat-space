0369a9e4f1499e9f72e694d81fe131b375ea759e
rockbook1105 <rockbook1105.xxx@gmail.com>
2018-06-09 15:14:59 +0900

    modified controller files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.json
+      format.html
+    end
+  end
   def edit
   end
 
