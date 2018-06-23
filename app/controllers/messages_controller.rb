26c0d152a34210273371f53759d47dfc06691a1b
rockbook1105 <rockbook1105.xxx@gmail.com>
2018-06-23 20:57:22 +0900

    describe APIcontroller format

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,28 +4,28 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
   end
 
   def create
     @message = current_user.messages.new(message_params)
     if @message.save
       respond_to do |format|
+        format.html { redirect_to group_messages_path(@group) }
         format.json
-        format.html {redirect_to group_messages_path(@group)}
       end
     else
+      # @messages = @group.messages.includes(:user)
       redirect_to group_messages_path(@group), notice: 'メッセージを入力してください'
     end
   end
-
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(group_id: params[:group_id])
+    params.require(:message).permit(:content, :image).merge(group_id: params[:group_id], user_id: params[:user_id])
   end
 
   def set_group
