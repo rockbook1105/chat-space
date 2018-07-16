685abe66424242cbc2461708f670bbd0599a0626
rockbook1105 <rockbook1105.xxx@gmail.com>
2018-07-16 22:34:11 +0900

    自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
     if (message.image.url == null){
       message.image.url = ""
     }
-      var html = `<div class= "message">
+      var html = `<div class= "message" data-message-id="${message.id}">
                     <div class="upper-message">
                       <div class="upper-message__user-name">${ message.user_name }
                       </div>
@@ -40,11 +40,43 @@ $(function(){
       .done(function(data) {
         var html = buildHTML(data);
         $(".messages").append(html);
-        console.log(html)
         $('#new_message').prop("disable", true);
         $('#new_message')[0].reset();
         scrollMessage()
       });
     });
-});
 
+  $(function(){
+    if(location.href.match(/\/groups\/\d+\/messages/)){
+      setInterval(messageUpdate, 5000);
+    }
+  });
+    function messageUpdate(){
+      if($('.message')[0]){
+        var message_id = $('.message').last().data('message-id');
+        console.log(message_id)
+      }else{
+        return false
+      }
+
+    $.ajax({
+      url: location.href,
+      type: "GET",
+      data: { id: message_id },
+      dataType: 'json'
+    })
+    .done(function(messages){
+      console.log(messages)
+      if(messages.length){
+      $.each(messages, function(i, messagge){
+        var insert_html = buildHTML(message);
+        $('.messages').append(insert_html);
+        scrollMessage()
+      });
+    }
+    })
+    .fail(function(data){
+      alert('メッセージの更新に失敗しました')
+    })
+  }
+});
