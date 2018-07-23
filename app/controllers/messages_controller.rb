1774c9682cee7d3f7fe152d359cc1450ef41cab6
rockbook1105 <rockbook1105.xxx@gmail.com>
2018-07-23 16:53:34 +0900

    コントローラーのテンプレート変数を削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    @users = @group.users
     respond_to do |format|
       format.html
       format.json { @new_messages = @messages.where('id > ?', params[:id]) }
