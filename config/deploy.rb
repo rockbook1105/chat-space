a3dc437fc9a9571bb6a36b74c6dd93a3fbae12bb
rockbook1105 <rockbook1105.xxx@gmail.com>
2018-08-03 15:45:27 +0900

    s3用の設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "chat-space"
 set :repo_url, "git@github.com/rockbook1105/chat-space"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
@@ -21,7 +22,20 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
+
 
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
